a4cfe2778af114d5f25d93277c0fa7d01c51777c
baiqiu.wsn <baiqiu.wsn@raycloud.com>
2023-03-23 20:17:03 +0800

    上传

--- app/core/api/mongodb/match-rules.ts
+++ app/core/api/mongodb/match-rules.ts
@@ -1,5 +1,5 @@
 import { model, connect } from 'mongoose'
-import { matchRuleSchema } from './match-rules-schema'
+import { I_MatchRuls, matchRuleSchema } from './match-rules-schema'
 
 const MatchRule = model('MatchRule', matchRuleSchema)
 
@@ -9,7 +9,45 @@ const MatchRule = model('MatchRule', matchRuleSchema)
  * @returns {any}
  */
 export async function getALlMatchRule() {
-  const res = await MatchRule.find({}).lean()
+  try {
+    const res = await MatchRule.find({}).lean()
 
-  return res
+    return getId(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function getId(data: any) {
+  return data.map((val: any) => {
+    return {
+      ...val,
+      m_id: val._id.toString(),
+    }
+  })
+}
+
+/**
+ * 查询每个月总支出
+ * @param {any} param:{start:any;end:any}
+ * @returns {any}
+ */
+export function UpdateOne(param: I_MatchRuls & { m_id: string }) {
+  const filter = { _id: param.m_id }
+  const update: Partial<I_MatchRuls & { m_id: string; _id: any }> = { ...param }
+  delete update.m_id
+  delete update._id
+  delete update.creation_time
+  delete update.modification_time
+  console.log(filter, 'filter')
+  console.log(update, 'update')
+  return new Promise((resolve, reject) => {
+    MatchRule.findOneAndUpdate(filter, update)
+      .then(() => {
+        resolve({ code: 200 })
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
 }
--- app/src/views/about/rule-table.tsx
+++ app/src/views/about/rule-table.tsx
@@ -4,7 +4,7 @@ import {
   cost_type,
   tag_type,
 } from '@/core/api/const/web'
-import { Space, Table, Tag } from 'antd'
+import { Input, message, Modal, Space, Table, Tag } from 'antd'
 import type { ColumnsType } from 'antd/es/table'
 import React, { useEffect, useState } from 'react'
 import { getCategoryString } from '@/core/api/const/category'
@@ -16,94 +16,6 @@ interface DataType {
   address: string
   tags: string[]
 }
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: '分类',
-    dataIndex: 'category',
-    width: 120,
-    render: (val: string) => {
-      const list = val ? JSON.parse(val) : []
-      console.log(list, 'list')
-      return list.length > 0 ? getCategoryString(list) : ''
-    },
-  },
-  {
-    title: '对象',
-    width: 80,
-    dataIndex: 'consumer',
-    key: 'consumer',
-    render: (val: number) => {
-      const consumer_type = {
-        1: '老公',
-        2: '老婆',
-        3: '家庭',
-        4: '牧牧',
-      }
-      if (val === 1) {
-        return <Tag color="cyan">{consumer_type[val]}</Tag>
-      } else if (val === 2) {
-        return <Tag color="magenta">{consumer_type[val]}</Tag>
-      } else if (val === 3) {
-        return <Tag color="geekblue">{consumer_type[val]}</Tag>
-      } else if (val === 4) {
-        return <Tag color="orange">{consumer_type[val]}</Tag>
-      }
-    },
-  },
-  {
-    title: '标签',
-    dataIndex: 'tag',
-    width: 80,
-    render: (val: number) => (val ? tag_type[val] : ''),
-  },
-  {
-    title: '规则',
-    dataIndex: 'rule',
-  },
-  {
-    title: '账户',
-    dataIndex: 'account_type',
-    width: 80,
-    render: (val: number) => (val ? account_type[val] : ''),
-  },
-  {
-    title: 'ABC类',
-    dataIndex: 'abc_type',
-    width: 80,
-    render: (val: number) => (val ? abc_type[val] : ''),
-  },
-  {
-    title: '消费方式',
-    dataIndex: 'cost_type',
-    width: 120,
-    render: (val: number) => (val ? cost_type[val] : ''),
-  },
-]
-
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-]
 
 const RuleTable = () => {
   const [ruleData, setRuleData] = useState<any>()
@@ -118,9 +30,125 @@ const RuleTable = () => {
       console.log(error)
     }
   }
+  const columns: ColumnsType<DataType> = [
+    {
+      title: '分类',
+      dataIndex: 'category',
+      width: 120,
+      render: (val: string) => {
+        const list = val ? JSON.parse(val) : []
+
+        return list.length > 0 ? getCategoryString(list) : ''
+      },
+    },
+    {
+      title: '规则',
+      dataIndex: 'rule',
+      render: (ru: string) => {
+        const arr = ru.split('|')
+        return arr.map((str: string, key: number) => {
+          return <Tag key={key}>{str}</Tag>
+        })
+      },
+    },
+    {
+      title: '对象',
+      width: 80,
+      dataIndex: 'consumer',
+      key: 'consumer',
+      render: (val: number) => {
+        const consumer_type = {
+          1: '老公',
+          2: '老婆',
+          3: '家庭',
+          4: '牧牧',
+        }
+        if (val === 1) {
+          return <Tag color="cyan">{consumer_type[val]}</Tag>
+        } else if (val === 2) {
+          return <Tag color="magenta">{consumer_type[val]}</Tag>
+        } else if (val === 3) {
+          return <Tag color="geekblue">{consumer_type[val]}</Tag>
+        } else if (val === 4) {
+          return <Tag color="orange">{consumer_type[val]}</Tag>
+        }
+      },
+    },
+
+    {
+      title: '标签',
+      dataIndex: 'tag',
+      width: 90,
+      render: (val: number) => (val ? tag_type[val] : ''),
+    },
+    {
+      title: 'ABC类',
+      dataIndex: 'abc_type',
+      width: 80,
+      render: (val: number) => (val ? abc_type[val] : ''),
+    },
+    {
+      title: '消费方式',
+      dataIndex: 'cost_type',
+      width: 100,
+      render: (val: number) => (val ? cost_type[val] : ''),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      width: 120,
+      render: (_, record) => (
+        <Space size="middle">
+          <a
+            onClick={() => {
+              setVisiable(true)
+              setRecord(record)
+              console.log(record)
+            }}
+          >
+            新增
+          </a>
+          <i className="ri-edit-box-line"></i>
+        </Space>
+      ),
+    },
+  ]
   useEffect(() => {
     getRuleData()
   }, [])
-  return <Table columns={columns} dataSource={ruleData} />
+  const [visiable, setVisiable] = useState(false)
+  const [input, setInput] = useState<string>('')
+  const [record, setRecord] = useState<any>()
+  const onSubmit = async () => {
+    if (!input) {
+      message.info('比天')
+      return
+    }
+    try {
+      const res = await $api.UpdateOne({
+        ...record,
+        rule: record.rule.concat(`|${input}`),
+      })
+      console.log(res, 'res')
+    } catch (error) {}
+  }
+  return (
+    <>
+      <Table columns={columns} dataSource={ruleData} scroll={{ x: 1500 }} />
+      <Modal
+        title="Basic Modal"
+        open={visiable}
+        onOk={onSubmit}
+        onCancel={() => setVisiable(false)}
+      >
+        <Input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value)
+          }}
+        />
+      </Modal>
+    </>
+  )
 }
 export default RuleTable
